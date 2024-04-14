82ae04dfc1c2b0504749c000c6c02313701fec6f
Ng wee liang <weeliangng@hotmail.com>
2024-04-15 01:09:37 +0800

    refactored assign cluster and now able to write on bigquery table from local

--- assigncluster.py
+++ assigncluster.py
@@ -1,8 +1,7 @@
-import geopandas as gpd
+from geopandas import GeoDataFrame
 from google.cloud import bigquery
-import plotly.express as px
 from sklearn.cluster import DBSCAN
-import pandas as pd
+
 
 def get_bigquery_taxi_data(date_str):
     dataset_name = 'taxi_availability'
@@ -16,22 +15,38 @@ def get_bigquery_taxi_data(date_str):
     ORDER BY timestamp ASC
     """
     client = bigquery.Client()
-
-    df = client.query(query).to_geodataframe()
+    job = client.query(query)
+    result = job.result()
+    df = result.to_geodataframe()
     if df.empty:
         raise ValueError(f"No data available for the specified date: {date_str}")
     else: return df
 
+def prepare_data(df):
+    df.timestamp = df.timestamp.dt.tz_convert('Asia/Singapore')
+    df_exploded = df.explode(index_parts=False)
+    df_exploded["longitude"], df_exploded["latitude"] = df_exploded.taxi_availability.x, df_exploded.taxi_availability.y
+    return df_exploded
+    
+def write_cluster_data(df):
+    client = bigquery.Client()
+    dataset_name = 'taxi_availability'
+    table_name = 'taxi_availability_cluster'
+    project_id = 'traffic-analysis-418408'
+    table_id = "{0}.{1}.{2}".format(project_id, dataset_name, table_name)
+    job_config = bigquery.LoadJobConfig(schema=[bigquery.SchemaField("timestamp", "TIMESTAMP"),
+                                                bigquery.SchemaField("taxi_count","INTEGER"),
+                                                bigquery.SchemaField("longitude", "FLOAT"),
+                                                bigquery.SchemaField("latitude", "FLOAT"),
+                                                bigquery.SchemaField("cluster", "INTEGER")])
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
 
 def assign_cluster(date_str):
     try: 
         df = get_bigquery_taxi_data(date_str)
     except ValueError as e:
-        print(e)
-    df.timestamp = df.timestamp.dt.tz_convert('Asia/Singapore')
-
-    df_exploded = df.explode(index_parts=False)
-    df_exploded["longitude"], df_exploded["latitude"] = df_exploded.taxi_availability.x, df_exploded.taxi_availability.y
+        raise (e)
+    df_exploded = prepare_data(df)
 
     for timestamp in df_exploded['timestamp'].unique():
         subset = df_exploded.loc[df_exploded.timestamp == timestamp].copy()
@@ -41,4 +56,8 @@ def assign_cluster(date_str):
         df_exploded.loc[df_exploded['timestamp'] == timestamp, 'cluster'] = subset['cluster']
     #df_exploded = df_exploded.loc[df_exploded.cluster != -1]
     #df_exploded["cluster"] = df_exploded["cluster"].map(str)
+    write_cluster_data(df_exploded[["timestamp", "taxi_count", "longitude", "latitude", "cluster"]])
+    
+
+
 
